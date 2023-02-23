b888d03f153086c39fae2cc134274e361bae0b34
OnlySig <gabriel.castro.pereira11@gmail.com>
2023-02-23 18:00:47 -0300

    Projeto de um site gastronômico feito em react

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Filtros from '../Filtros';
+import styles from './Cardapio.module.scss'
+import { ReactComponent as Logo } from '../assets/img/logo.svg';
+import Buscador from 'pages/Buscador';
+import { useState } from 'react';
+import Ordenador from './Ordenador';
+import Itens from './Itens';
+
+export default function Cardapio() {
+  const [busca, setBusca] = useState("");
+  const [filtro, setFiltro] = useState<number | null>(null);
+  const [ordenador, setOrdenador] = useState("");
+
+  return (
+    <main>
+      <nav className={styles.menu}>
+        <Logo />
+      </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>
+          A casa do código e da massa
+        </div>
+      </header>
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+        <Buscador
+          busca={busca} 
+          setBusca={setBusca}
+        />
+        <div className={styles.cardapio__filtros}>
+          <Filtros filtro={filtro} setFiltro={setFiltro}/>
+          <div className={styles.cardapio__ordeiro}>
+            <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}/>
+          </div>
+        </div>
+          <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
+      </section>
+    </main>
+  )
+}
